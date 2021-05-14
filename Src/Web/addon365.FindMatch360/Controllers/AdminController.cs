30873dc045c53fdd07374beb037aeedb990b262b
venkatesanid <venkatesanaddon365@outlook.com>
2021-05-14 17:48:33 +0530

    admin pages added and link
    1) index
    2)paid list
    3)payment option

---
+++ Src/Web/addon365.FindMatch360/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace addon365.FindMatch360.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult PaidList()
+        {
+            return View();
+        }
+
+        public IActionResult paymentoptions()
+        {
+            return View();
+        }
+    }
+}
