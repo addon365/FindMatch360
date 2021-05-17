4a2d15cc4e8c79af2bc29ace18937ddcece1afaf
venkatesanid <venkatesanaddon365@outlook.com>
2021-05-17 16:31:43 +0530

    create file
    I added to create file

--- Src/Web/addon365.FindMatch360/Controllers/AdminController.cs
+++ Src/Web/addon365.FindMatch360/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,12 @@ namespace addon365.FindMatch360.Controllers
         }
 
         public IActionResult paymentoptions()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult CreateProfile()
         {
             return View();
         }
