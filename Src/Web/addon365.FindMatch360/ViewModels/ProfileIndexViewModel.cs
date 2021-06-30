a0f5136467fc234cb87e9a03a668bd7658b3e216
tamilselvanid <tamilselvanid@outlook.com>
2021-06-30 13:16:05 +0530

    DateofBirth to Age Implementation

--- Src/Web/addon365.FindMatch360/ViewModels/ProfileIndexViewModel.cs
+++ Src/Web/addon365.FindMatch360/ViewModels/ProfileIndexViewModel.cs
@@ -10,7 +10,21 @@ namespace addon365.FindMatch360.ViewModels
         public Guid ProfileMasterId { get; set; }
         public string PhotoUrl { get; set; }
         public string ProfileName { get; set; }
-        public int Age { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string Age { get
+            {
+                // Save today's date.
+                var today = DateTime.Today;
+
+                // Calculate the age.
+                var age = today.Year - DateofBirth.Year;
+
+                // Go back to the year in which the person was born in case of a leap year
+                if (DateofBirth.Date > today.AddYears(-age)) age--;
+
+                return age.ToString();
+            }
+        }
         public string Star { get; set; }
         public string EducationQualification { get; set; }
         public string JobDetail { get; set; }
