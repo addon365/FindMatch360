3dc9ba5bb14f8b3c31f5d76b4d78872467dea5a2
venkatesanid <venkatesanaddon365@outlook.com>
2021-07-01 18:18:35 +0530

    adminuser
    I added to admin user

---
+++ Src/Web/addon365.FindMatch360/Controllers/AdminUserController.cs
@@ -0,0 +1,51 @@
+﻿using addon365.FindMatch360.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace addon365.FindMatch360.Controllers
+{
+    public class AccountAdminController : Controller
+    {
+        public AccountAdminController()
+        {
+
+
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(AccountAdminCreateViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Src/Web/addon365.FindMatch360/Models/AsccountUser/AccountUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace addon365.FindMatch360.Models.AsccountUser
+{
+    public class AccountUser
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String confirmpassword { get; set; }
+        public String comment { get; set; }
+    }
+}
