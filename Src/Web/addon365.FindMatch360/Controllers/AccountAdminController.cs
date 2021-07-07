5626c1078fc95f7d5a8b9c5b0b6a728f246d5d42
venkatesanid <venkatesanaddon365@outlook.com>
2021-07-07 17:33:15 +0530

    upgrade Business plan
    I added upgrade Business plan

--- Src/Web/addon365.FindMatch360/Controllers/AccountAdminController.cs
+++ Src/Web/addon365.FindMatch360/Controllers/AccountAdminController.cs
@@ -14,10 +14,12 @@ namespace addon365.FindMatch360.Controllers
     public class AccountAdminController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountAdminController(UserManager<ApplicationUser> userManager)
+        public AccountAdminController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             this._userManager = userManager;
+            this._signInManager = signInManager;
         }
         public IActionResult Create()
         {
@@ -41,6 +43,45 @@ namespace addon365.FindMatch360.Controllers
 
                 return RedirectToAction(nameof(Index));
             }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return RedirectToAction("Login");
+                }
+
+                // ChangePasswordAsync changes the user password
+                var result = await _userManager.ChangePasswordAsync(user,
+                    model.CurrentPassword, model.NewPassword);
+
+                // The new password did not meet the complexity rules or
+                // the current password is incorrect. Add these errors to
+                // the ModelState and rerender ChangePassword view
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+
+                // Upon successfully changing the password refresh sign-in cookie
+                await _signInManager.RefreshSignInAsync(user);
+                return View("ChangePasswordConfirmation");
+            }
+
             return View(model);
         }
         public async Task<IActionResult> Index()
